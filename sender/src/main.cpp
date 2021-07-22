2de91bbbc4a9cc28d946cf7df4dd040d29d98a1a
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-07-21 20:16:53 -0500

    ToF data successfuly sends with LoRa

--- sender/src/main.cpp
+++ sender/src/main.cpp
@@ -10,10 +10,8 @@
 
 #define BAND 915E6  // You can set band here directly,e.g. 868E6,915E6
 
-
 Adafruit_VL53L0X ToF = Adafruit_VL53L0X();
 
-unsigned int counter = 0;
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
@@ -43,31 +41,31 @@ void setup(){
 void loop(){
 
   VL53L0X_RangingMeasurementData_t val = read_ToF(ToF);
+  bool ToF_flag;
 
   if (val.RangeStatus != 4) {  // phase failures have incorrect data
-      Serial.print("Distance (mm): "); 
-      Serial.println(val.RangeMilliMeter);
+    ToF_flag = true;
+      // Serial.print("Distance (mm): "); 
+      // Serial.println(val.RangeMilliMeter);
   } 
   else {
-      Serial.println(" Out of range ");
+    ToF_flag = false;
+    // Serial.println(" Out of range ");
   }
 
-  // send packet
+  // Send packet
+  // TODO: make into a separate function
   LoRa.beginPacket();
-  
-// /*
-//  * LoRa.setTxPower(txPower,RFOUT_pin);
-//  * txPower -- 0 ~ 20
-//  * RFOUT_pin could be RF_PACONFIG_PASELECT_PABOOST or RF_PACONFIG_PASELECT_RFO
-//  *   - RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
-//  *   - RF_PACONFIG_PASELECT_RFO     -- LoRa single output via RFO_HF / RFO_LF, maximum output 14dBm
-// */
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
-  LoRa.print("hello");
-  LoRa.print(counter);
+  if (ToF_flag){
+    LoRa.print("Distance = ");
+    LoRa.print(val.RangeMilliMeter);
+  }
+  else{
+    LoRa.print("Distance = ???");
+  }
   LoRa.endPacket();
 
-  counter++;
-  delay(1000);
+  delay(100);
 
 }
