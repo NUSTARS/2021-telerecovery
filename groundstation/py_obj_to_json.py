a07b114e2299c1e917f4e74421c81add770eaab7
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-07-25 10:34:35 -0500

    update

--- groundstation/py_obj_to_json.py
+++ groundstation/py_obj_to_json.py
@@ -1,5 +1,14 @@
 import json
 
+'''
+# Read serial port with something like this 
+BAUD = 115200
+COM_PORT = 'COM4'
+with serial.Serial(port=COM_PORT, baudrate=BAUD, timeout=1) as ser:
+  x = ser.read()          # read one byte
+  s = ser.read(10)        # read up to ten bytes (timeout)
+  line = ser.readline()   # read a '\n' terminated line
+'''
 # a Python object (dict):
 x = {
   "Time": "00:00:01",
