33e6dd2b305cea8066ffc14bc591ad7f0421f674
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-07-28 21:38:07 -0500

    update

--- reciever/src/LoRa_RX.cpp
+++ reciever/src/LoRa_RX.cpp
@@ -10,9 +10,9 @@ void cbk(int packetSize) {
   }
 
   rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
-  
+
   print_LoRa_data(0,26,128,packet,rssi);
 
-  Serial.println(packet);
+  Serial.println(packet); // Print packet over UART -> to JS app
 }
 
