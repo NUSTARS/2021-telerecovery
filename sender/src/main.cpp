90f94a81b606bc2a6a044d26d1b2ce8ec9a2b8b3
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-09-13 12:41:23 -0500

    updates

--- sender/src/main.cpp
+++ sender/src/main.cpp
@@ -5,13 +5,29 @@
 #include <Arduino.h>
 #include "heltec.h"
 #include "ToF.h"
+#include "IMU.h"
 
 #define BAND 915E6  // You can set band here directly,e.g. 868E6,915E6
 
+#define DT  0.02          // Loop time
+
+
 Adafruit_VL53L0X ToF = Adafruit_VL53L0X();
+
+byte buff[6];
+int accRaw[3];
+int gyrRaw[3];
+float gyroXangle = 0.0;
+float gyroYangle = 0.0;
+float gyroZangle = 0.0;
+float AccYangle = 0.0;
+float AccXangle = 0.0;
+
+unsigned long starttime;
 
 void setup(){
 
+  // Start ESP32 Board
   Heltec.begin(
     false /*DisplayEnable Enable*/,
     true /*Heltec.Heltec.Heltec.LoRa Disable*/, 
@@ -26,9 +42,39 @@ void setup(){
       Serial.println(F("Failed to boot VL53L0X"));
       while(1);
   }
+
+  // Initialize the IMU
+  detectIMU();
+  enableIMU();
+
+  Serial.begin(115200); // init serial for testing
+  delay(500);
+
+  starttime = millis();
+
 }
 
 void loop(){
+
+  unsigned long timenow = millis();
+
+  //Read the measurements from  sensors
+  readACC(buff);
+  accRaw[0] = (int)(buff[0] | (buff[1] << 8));   
+  accRaw[1] = (int)(buff[2] | (buff[3] << 8));
+  accRaw[2] = (int)(buff[4] | (buff[5] << 8));
+
+  //Convert Accelerometer values to degrees
+  AccXangle = (float) (atan2(accRaw[1],accRaw[2])+M_PI)*RAD_TO_DEG;
+  AccYangle = (float) (atan2(accRaw[2],accRaw[0])+M_PI)*RAD_TO_DEG;
+
+  //If IMU is up the correct way, use these lines
+        AccXangle -= (float)180.0;
+  if (AccYangle > 90)
+          AccYangle -= (float)270;
+  else
+    AccYangle += (float)90;
+
 
   VL53L0X_RangingMeasurementData_t val = read_ToF(ToF);
   bool ToF_flag;
@@ -40,19 +86,37 @@ void loop(){
     ToF_flag = false;
   }
 
-  // Send packet
-  LoRa.beginPacket();
-  LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
+  Serial.print("AccX=");
+  Serial.print(AccXangle);
+  Serial.print("\tAccY=");
+  Serial.print(AccYangle);
+  Serial.print("\tDistance=");
+
+
   if (ToF_flag){
-    LoRa.print("Distance = ");
-    LoRa.print(val.RangeMilliMeter);
-    LoRa.print(" mm ");
+    Serial.print(val.RangeMilliMeter);
   }
   else{
-    LoRa.print("Distance = ??");
+    Serial.print("?");
   }
-  LoRa.endPacket();
 
-  delay(100);
+  Serial.print("\tTime=");
+  Serial.println(starttime-timenow);
+
+
+  // // Send packet
+  // LoRa.beginPacket();
+  // LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
+  // if (ToF_flag){
+  //   LoRa.print("Distance = ");
+  //   LoRa.print(val.RangeMilliMeter);
+  //   LoRa.print(" mm ");
+  // }
+  // else{
+  //   LoRa.print("Distance = ??");
+  // }
+  // LoRa.endPacket();
+
+  // delay(100);
 
 }
