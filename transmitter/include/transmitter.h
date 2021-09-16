15677eb0afaa9f905b26c176d8be81997c20118d
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-09-15 23:37:04 -0500

    package data into array first, then print, and other updates

--- transmitter/include/transmitter.h
+++ transmitter/include/transmitter.h
@@ -11,5 +11,3 @@
 #ifndef M_PI
   #define M_PI 3.141592653589793
 #endif
-
-#define DT  0.02          // Loop time
--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -1,7 +1,7 @@
 #include "transmitter.h"
 
 Adafruit_VL53L0X ToF = Adafruit_VL53L0X(); // Declare time of flight sensor object
-Adafruit_BMP3XX bmp; // Declare pressure sensor object
+Adafruit_BMP3XX bmp;                       // Declare pressure sensor object
 
 unsigned char buff[6];
 int accRaw[3];
@@ -13,22 +13,25 @@ float gyroZangle = 0.0;
 float AccYangle = 0.0;
 float AccXangle = 0.0;
 
-void setup(){
+void setup()
+{
 
   // Start ESP32 Board
   Heltec.begin(
-    false /*DisplayEnable Enable*/,
-    true /*Heltec.Heltec.Heltec.LoRa Disable*/, 
-    true /*Serial Enable*/, 
-    true /*PABOOST Enable*/, 
-    BAND /*long BAND*/);
- 
+      false /*DisplayEnable Enable*/,
+      true /*Heltec.Heltec.Heltec.LoRa Disable*/,
+      true /*Serial Enable*/,
+      true /*PABOOST Enable*/,
+      BAND /*long BAND*/);
+
   delay(1000); // Maybe do not need this or can be shorter?
 
   // Initialize time of flight sensor
-  if (!ToF.begin()) {
-      Serial.println(F("Failed to boot VL53L0X"));
-      while(1);
+  if (!ToF.begin())
+  {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while (1)
+      ;
   }
 
   // Initialize the IMU
@@ -36,11 +39,13 @@ void setup(){
   enableIMU();
 
   Serial.begin(115200); // init serial for testing
-  delay(500);
+  delay(50);
 
-   if (!bmp.begin_I2C()) {
+  if (!bmp.begin_I2C())
+  {
     Serial.println("Failed to boot BMP388");
-    while (1);
+    while (1)
+      ;
   }
 
   // Set up oversampling and filter initialization on the BMP388
@@ -48,58 +53,57 @@ void setup(){
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-
 }
 
-void loop(){
+void loop()
+{
 
-  if (! bmp.performReading()) {
+  if (!bmp.performReading())
+  {
     Serial.println("Failed to perform reading :(");
     return;
   }
 
   //Read the measurements from  sensors
   readACC(buff);
-  accRaw[0] = (int)(buff[0] | (buff[1] << 8));   
+  accRaw[0] = (int)(buff[0] | (buff[1] << 8));
   accRaw[1] = (int)(buff[2] | (buff[3] << 8));
   accRaw[2] = (int)(buff[4] | (buff[5] << 8));
 
   //Convert Accelerometer values to degrees
-  AccXangle = (float) (atan2(accRaw[1],accRaw[2])+M_PI)*RAD_TO_DEG;
-  AccYangle = (float) (atan2(accRaw[2],accRaw[0])+M_PI)*RAD_TO_DEG;
+  AccXangle = (float)(atan2(accRaw[1], accRaw[2]) + M_PI) * RAD_TO_DEG;
+  AccYangle = (float)(atan2(accRaw[2], accRaw[0]) + M_PI) * RAD_TO_DEG;
 
   //If IMU is up the correct way, use these lines
   AccXangle -= (float)180.0;
   if (AccYangle > 90)
-          AccYangle -= (float)270;
+  {
+    AccYangle -= (float)270;
+  }
   else
+  {
     AccYangle += (float)90;
-
+  }
 
   VL53L0X_RangingMeasurementData_t val = read_ToF(ToF);
-  bool ToF_flag;
 
-  if (val.RangeStatus != 4) {  // phase failures have incorrect data
-    ToF_flag = true;
-  } 
-  else {
-    ToF_flag = false;
+  uint16_t tof_data;
+  if (val.RangeStatus != 4)
+  {
+    tof_data = val.RangeMilliMeter;
+  }
+  else
+  {
+    tof_data = 0;
   }
 
-  Serial.print(AccXangle);
-  Serial.print(AccYangle);
-  
-  if (ToF_flag){
-    Serial.print(val.RangeMilliMeter);
-  }
-  else{
-    Serial.print(0);
-  }
-  Serial.print(bmp.temperature);
-  Serial.print(bmp.pressure / 100.0);
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  delay(DT);
+  // Pack up the data into an array
+  char buffer[50];
+  char data_format[] = "%f, %f, %hu, %f, %f, %f";
+  sprintf(buffer, data_format, AccXangle, AccYangle, tof_data, bmp.temperature, bmp.pressure, bmp.readAltitude(SEALEVELPRESSURE_HPA));
 
+  Serial.println(buffer); // Printing all the data to test (change to LoRa print)
+  delay(200);
 
   // // Send packet
   // LoRa.beginPacket();
@@ -115,5 +119,4 @@ void loop(){
   // LoRa.endPacket();
 
   // delay(100);
-
 }
