3aa6775554f28db65c07913a64f8f21295f4dc11
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-09-16 19:32:53 -0500

    added gyro and CF angles to data, transmission is also faster now

--- transmitter/include/transmitter.h
+++ transmitter/include/transmitter.h
@@ -8,6 +8,13 @@
 #define BAND 915E6  // You can set band here directly,e.g. 868E6,915E6
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+#define DT  0.02          // Loop time
+#define AA  0.97         // complementary filter constant
+#define G_GAIN 0.070    // [deg/s/LSB]  
+
+
 #ifndef M_PI
   #define M_PI 3.141592653589793
 #endif
+
+
--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -3,6 +3,7 @@
 Adafruit_VL53L0X ToF = Adafruit_VL53L0X(); // Declare time of flight sensor object
 Adafruit_BMP3XX bmp;                       // Declare pressure sensor object
 
+
 unsigned char buff[6];
 int accRaw[3];
 int gyrRaw[3];
@@ -10,9 +11,18 @@ int gyrRaw[3];
 float gyroXangle = 0.0;
 float gyroYangle = 0.0;
 float gyroZangle = 0.0;
+
+float rate_gyr_y = 0.0;   // [deg/s]
+float rate_gyr_x = 0.0;    // [deg/s]
+float rate_gyr_z = 0.0;     // [deg/s]
+
 float AccYangle = 0.0;
 float AccXangle = 0.0;
 
+float CFangleX = 0.0;
+float CFangleY = 0.0;
+
+unsigned long startTime;
 void setup()
 {
 
@@ -27,25 +37,22 @@ void setup()
   delay(500); // Maybe do not need this or can be shorter?
 
   // Initialize time of flight sensor
-  if (!ToF.begin())
-  {
-    // Serial.println(F("Failed to boot VL53L0X"));
-    while (1)
-      ;
+  if (!ToF.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while (1);
   }
+
+
+  Serial.begin(115200); // init serial for testing
+  delay(500);
 
   // Initialize the IMU
   detectIMU();
   enableIMU();
 
-  // Serial.begin(115200); // init serial for testing
-  // delay(50);
-
-  if (!bmp.begin_I2C())
-  {
-    // Serial.println("Failed to boot BMP388");
-    while (1)
-      ;
+  if (!bmp.begin_I2C()) {
+    Serial.println("Failed to boot BMP388");
+    while (1);
   }
 
   // Set up oversampling and filter initialization on the BMP388
@@ -55,18 +62,24 @@ void setup()
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
-void loop()
-{
-  if (!bmp.performReading())
-  {
+void loop() {
+  startTime = millis();
+
+  if (!bmp.performReading()) {
     return;
   }
 
-  //Read the measurements from  sensors
+  // Read raw acceleration data
   readACC(buff);
   accRaw[0] = (int)(buff[0] | (buff[1] << 8));
   accRaw[1] = (int)(buff[2] | (buff[3] << 8));
   accRaw[2] = (int)(buff[4] | (buff[5] << 8));
+
+  // Read raw gyroscope data
+  readGYR(buff);
+  gyrRaw[0] = (int)(buff[0] | (buff[1] << 8));   
+  gyrRaw[1] = (int)(buff[2] | (buff[3] << 8));
+  gyrRaw[2] = (int)(buff[4] | (buff[5] << 8));
 
   //Convert Accelerometer values to degrees
   AccXangle = (float)(atan2(accRaw[1], accRaw[2]) + M_PI) * RAD_TO_DEG;
@@ -83,6 +96,21 @@ void loop()
     AccYangle += (float)90;
   }
 
+  //Convert Gyro raw to degrees per second
+  rate_gyr_x = (float) gyrRaw[0] * G_GAIN;
+  rate_gyr_y = (float) gyrRaw[1]  * G_GAIN;
+  rate_gyr_z = (float) gyrRaw[2]  * G_GAIN;
+
+  //Calculate the angles from the gyro
+  gyroXangle+=rate_gyr_x*DT;
+  gyroYangle+=rate_gyr_y*DT;
+  gyroZangle+=rate_gyr_z*DT;
+
+  //Complementary filter used to combine the accelerometer and gyro values.
+  CFangleX=AA*(CFangleX+rate_gyr_x*DT) +(1 - AA) * AccXangle;
+  CFangleY=AA*(CFangleY+rate_gyr_y*DT) +(1 - AA) * AccYangle;
+
+
   VL53L0X_RangingMeasurementData_t val = read_ToF(ToF);
 
   uint16_t tof_data=0;
@@ -96,15 +124,29 @@ void loop()
   }
 
   // Pack up the data into an array
-  char buffer[256];
-  char data_format[] = "%f,%f,%hu,%f,%f,%f";
-  sprintf(buffer, data_format, AccXangle, AccYangle, tof_data, bmp.temperature, bmp.pressure, bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  char buffer[128];
+  char data_format[] = "%f,%f,%hu,%f,%f,%f,%f,%f,%f,%f,%f\n";
+  sprintf(buffer, data_format, 
+  AccXangle,
+  AccYangle,
+  tof_data,
+  bmp.temperature,
+  bmp.pressure,
+  bmp.readAltitude(SEALEVELPRESSURE_HPA),
+  gyroXangle,
+  gyroYangle,
+  gyroZangle,
+  CFangleX,
+  CFangleY);
 
+  // Serial.print(buffer);
   LoRa.beginPacket();
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
   LoRa.print(buffer);
   LoRa.endPacket();
 
-  // delay(20);
-  
+  //Each loop should be at least 20ms.
+  while(millis() - startTime < (DT*1000)) {
+    delay(1);
   }
+}
