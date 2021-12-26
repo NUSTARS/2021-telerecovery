5aab6718536970199b690c8830311f5471dd0825
Nick <nmarks99@gmail.com>
2021-12-25 22:41:36 -0600

    9(IMU)+4(ToF and BMP) dof sensors all output good data

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -1,23 +1,6 @@
-/***************************************************************************
-  This is an example for the Adafruit AHRS library
-  It will look for a supported magnetometer and output
-  PJRC Motion Sensor Calibration Tool-compatible serial data
-
-  PJRC & Adafruit invest time and resources providing this open source code,
-  please support PJRC and open-source hardware by purchasing products
-  from PJRC!
-
-  Written by PJRC, adapted by Limor Fried for Adafruit Industries.
- ***************************************************************************/
-
 #include <Adafruit_Sensor_Calibration.h>
-
-Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
-
-uint16_t crc16_update(uint16_t crc, uint8_t a);
-void receiveCalibration();
-
-
+#include "transmitter.h"
+Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer; // IMU
 
 // uncomment one combo 9-DoF!
 #include "LSM6DS_LIS3MDL.h"  // see the the LSM6DS_LIS3MDL file in this project to change board to LSM6DS33, LSM6DS3U, LSM6DSOX, etc
@@ -31,31 +14,76 @@ void receiveCalibration();
   Adafruit_Sensor_Calibration_SDFat cal;
 #endif
 
+
+// Declare sensor objects
+Adafruit_VL53L0X ToF = Adafruit_VL53L0X(); // time of flight
+Adafruit_BMP3XX bmp; // pressure                  
+
+// variables to store IMU data
 sensors_event_t mag_event, gyro_event, accel_event;
 
+unsigned long startTime; // time for loop
 int loopcount = 0;
 
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
-  
-  Serial.println(F("Adafruit AHRS - IMU Calibration!"));
+// function prototypes
+uint16_t crc16_update(uint16_t crc, uint8_t a);
+void receiveCalibration();
 
-  Serial.println("Calibration filesys test");
+
+
+void setup(void) {
+
+  // Start ESP32 Board
+  Heltec.begin(
+      false /*DisplayEnable Enable*/,
+      true /*LoRa enable*/,
+      true /*Serial Enable*/,
+      true /*PABOOST Enable*/,
+      BAND /*long BAND*/);
+  delay(500); 
+
+  Serial.begin(115200); // init serial for testings
+  while (!Serial) delay(10);  
+
+  // Initialize time of flight sensor
+  if (!ToF.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1) {;}
+  }
+
+  // Initialize BMP388 pressure sensor
+  if (!bmp.begin_I2C()) {
+    Serial.println("Failed to boot BMP388");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization on the BMP388
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+
+  // Initialize calibration helper code
   if (!cal.begin()) {
     Serial.println("Failed to initialize calibration helper");
     while (1) yield();
   }
+
+  // Load existing calibration from EEPROM if its there, or use default
   if (! cal.loadCalibration()) {
     Serial.println("No calibration loaded/found... will start with defaults");
   } else {
     Serial.println("Loaded existing calibration");
   }
 
+  // Initialize IMU sensors
   if (!init_sensors()) {
     Serial.println("Failed to find sensors");
     while (1) delay(10);
   }
+
+  // Print out sensor information
   accelerometer->printSensorDetails();
   gyroscope->printSensorDetails();
   magnetometer->printSensorDetails();
@@ -65,67 +93,124 @@ void setup(void) {
   Wire.setClock(400000); // 400KHz
 }
 
+
 void loop() {
+
+  startTime = millis();
+
+  // Read pressure sensor reading 
+  if (!bmp.performReading()) {
+    return;
+  }
+
+  // Read time of flight sensor data
+  VL53L0X_RangingMeasurementData_t val = read_ToF(ToF);
+  uint16_t tof_data=0;
+  if (val.RangeStatus != 4)
+  {
+    tof_data = val.RangeMilliMeter;
+  }
+  else
+  {
+    tof_data = 0;
+  }
+
+
+  // Read IMU data
   magnetometer->getEvent(&mag_event);
   gyroscope->getEvent(&gyro_event);
   accelerometer->getEvent(&accel_event);
   
-  // 'Raw' values to match expectation of MotionCal
-  Serial.print("Raw:");
-  Serial.print(int(accel_event.acceleration.x*8192/9.8)); Serial.print(",");
-  Serial.print(int(accel_event.acceleration.y*8192/9.8)); Serial.print(",");
-  Serial.print(int(accel_event.acceleration.z*8192/9.8)); Serial.print(",");
-  Serial.print(int(gyro_event.gyro.x*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
-  Serial.print(int(gyro_event.gyro.y*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
-  Serial.print(int(gyro_event.gyro.z*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
-  Serial.print(int(mag_event.magnetic.x*10)); Serial.print(",");
-  Serial.print(int(mag_event.magnetic.y*10)); Serial.print(",");
-  Serial.print(int(mag_event.magnetic.z*10)); Serial.println("");
+  // // 'Raw' values to match expectation of MotionCal
+  // Serial.print("Raw:");
+  // Serial.print(int(accel_event.acceleration.x*8192/9.8)); Serial.print(",");
+  // Serial.print(int(accel_event.acceleration.y*8192/9.8)); Serial.print(",");
+  // Serial.print(int(accel_event.acceleration.z*8192/9.8)); Serial.print(",");
+  // Serial.print(int(gyro_event.gyro.x*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
+  // Serial.print(int(gyro_event.gyro.y*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
+  // Serial.print(int(gyro_event.gyro.z*SENSORS_RADS_TO_DPS*16)); Serial.print(",");
+  // Serial.print(int(mag_event.magnetic.x*10)); Serial.print(",");
+  // Serial.print(int(mag_event.magnetic.y*10)); Serial.print(",");
+  // Serial.print(int(mag_event.magnetic.z*10)); Serial.println("");
 
   // unified data
-  Serial.print("Uni:");
-  Serial.print(accel_event.acceleration.x); Serial.print(",");
-  Serial.print(accel_event.acceleration.y); Serial.print(",");
-  Serial.print(accel_event.acceleration.z); Serial.print(",");
-  Serial.print(gyro_event.gyro.x, 4); Serial.print(",");
-  Serial.print(gyro_event.gyro.y, 4); Serial.print(",");
-  Serial.print(gyro_event.gyro.z, 4); Serial.print(",");
-  Serial.print(mag_event.magnetic.x); Serial.print(",");
-  Serial.print(mag_event.magnetic.y); Serial.print(",");
-  Serial.print(mag_event.magnetic.z); Serial.println("");
+  // Serial.print("Uni:");
+
+  // Pack up the data into an array
+  char buffer[512];
+  char data_format[] = "ToF=%hu,T=%f,P=%f,Alt=%f\na_x%f,a_y%f,a_z%f\ng_x%f,g_y%f,g_z=%f\nm_x=%f,m_y=%f,m_z=%f\n\n";
+
+  sprintf(buffer,data_format,
+    tof_data,
+    bmp.temperature,
+    bmp.pressure,
+    bmp.readAltitude(SEALEVELPRESSURE_HPA),
+    accel_event.acceleration.x,
+    accel_event.acceleration.y,
+    accel_event.acceleration.z,
+    gyro_event.gyro.x,
+    gyro_event.gyro.y,
+    gyro_event.gyro.z,
+    mag_event.magnetic.x,
+    mag_event.magnetic.y,
+    mag_event.magnetic.z
+  );
+
+  Serial.print(buffer);
+
+  // Serial.print(accel_event.acceleration.x); Serial.print(",");
+  // Serial.print(accel_event.acceleration.y); Serial.print(",");
+  // Serial.print(accel_event.acceleration.z); Serial.print(",");
+  // Serial.print(gyro_event.gyro.x, 4); Serial.print(",");
+  // Serial.print(gyro_event.gyro.y, 4); Serial.print(",");
+  // Serial.print(gyro_event.gyro.z, 4); Serial.print(",");
+  // Serial.print(mag_event.magnetic.x); Serial.print(",");
+  // Serial.print(mag_event.magnetic.y); Serial.print(",");
+  // Serial.print(mag_event.magnetic.z); Serial.println("");
   loopcount++;
   receiveCalibration();
 
-  // occasionally print calibration
-  if (loopcount == 50 || loopcount > 100) {
-    Serial.print("Cal1:");
-    for (int i=0; i<3; i++) {
-      Serial.print(cal.accel_zerog[i], 3); 
-      Serial.print(",");
-    }
-    for (int i=0; i<3; i++) {
-      Serial.print(cal.gyro_zerorate[i], 3);
-      Serial.print(",");
-    }  
-    for (int i=0; i<3; i++) {
-      Serial.print(cal.mag_hardiron[i], 3); 
-      Serial.print(",");
-    }  
-    Serial.println(cal.mag_field, 3);
-    loopcount++;
-  }
-  if (loopcount >= 100) {
-    Serial.print("Cal2:");
-    for (int i=0; i<9; i++) {
-      Serial.print(cal.mag_softiron[i], 4); 
-      if (i < 8) Serial.print(',');
-    }
-    Serial.println();
-    loopcount = 0;
+  // // occasionally print calibration
+  // if (loopcount == 50 || loopcount > 100) {
+  //   Serial.print("Cal1:");
+  //   for (int i=0; i<3; i++) {
+  //     Serial.print(cal.accel_zerog[i], 3); 
+  //     Serial.print(",");
+  //   }
+  //   for (int i=0; i<3; i++) {
+  //     Serial.print(cal.gyro_zerorate[i], 3);
+  //     Serial.print(",");
+  //   }  
+  //   for (int i=0; i<3; i++) {
+  //     Serial.print(cal.mag_hardiron[i], 3); 
+  //     Serial.print(",");
+  //   }  
+  //   Serial.println(cal.mag_field, 3);
+  //   loopcount++;
+  // }
+  // if (loopcount >= 100) {
+  //   Serial.print("Cal2:");
+  //   for (int i=0; i<9; i++) {
+  //     Serial.print(cal.mag_softiron[i], 4); 
+  //     if (i < 8) Serial.print(',');
+  //   }
+  //   Serial.println();
+  //   loopcount = 0;
+  // }
+
+  // LoRa.beginPacket();
+  // LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
+  // LoRa.print(buffer);
+  // LoRa.endPacket();
+
+  //Each loop should be at least 20ms.
+  while(millis() - startTime < (DT*1000)) {
+    delay(1);
   }
  
   delay(10); 
-}
+
+} // END LOOP
 
 /********************************************************/
 
