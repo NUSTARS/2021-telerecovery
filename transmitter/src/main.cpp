b656e1da31db4e411e5d80bc906172fe2883f46d
Nick <nmarks99@gmail.com>
2021-12-28 14:26:21 -0600

    removed calibration stuff and added lora transmission

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -84,9 +84,9 @@ void setup(void) {
   }
 
   // Print out sensor information
-  accelerometer->printSensorDetails();
-  gyroscope->printSensorDetails();
-  magnetometer->printSensorDetails();
+  // accelerometer->printSensorDetails();
+  // gyroscope->printSensorDetails();
+  // magnetometer->printSensorDetails();
 
   setup_sensors();
   
@@ -123,7 +123,7 @@ void loop() {
   
   // Pack up the data into an array
   char buffer[512];
-  char data_format[] = "ToF=%hu,T=%f,P=%f,Alt=%f,a_x%f,a_y%f,a_z%f,g_x%f,g_y%f,g_z=%f,m_x=%f,m_y=%f,m_z=%f\n";
+  char data_format[] = "%hu,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n";
 
   sprintf(buffer,data_format,
     tof_data,
@@ -143,37 +143,6 @@ void loop() {
 
   // Serial.print(buffer);
 
-  loopcount++;
-  // receiveCalibration();
-
-  // // occasionally print calibration
-  // if (loopcount == 50 || loopcount > 100) {
-  //   Serial.print("Cal1:");
-  //   for (int i=0; i<3; i++) {
-  //     Serial.print(cal.accel_zerog[i], 3); 
-  //     Serial.print(",");
-  //   }
-  //   for (int i=0; i<3; i++) {
-  //     Serial.print(cal.gyro_zerorate[i], 3);
-  //     Serial.print(",");
-  //   }  
-  //   for (int i=0; i<3; i++) {
-  //     Serial.print(cal.mag_hardiron[i], 3); 
-  //     Serial.print(",");
-  //   }  
-  //   Serial.println(cal.mag_field, 3);
-  //   loopcount++;
-  // }
-  // if (loopcount >= 100) {
-  //   Serial.print("Cal2:");
-  //   for (int i=0; i<9; i++) {
-  //     Serial.print(cal.mag_softiron[i], 4); 
-  //     if (i < 8) Serial.print(',');
-  //   }
-  //   Serial.println();
-  //   loopcount = 0;
-  // }
-
   LoRa.beginPacket();
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
   LoRa.print(buffer);
@@ -187,105 +156,3 @@ void loop() {
   // delay(10); 
 
 } // END LOOP
-
-/********************************************************/
-
-// byte caldata[68]; // buffer to receive magnetic calibration data
-// byte calcount=0;
-
-// void receiveCalibration() {
-//   uint16_t crc;
-//   byte b, i;
-
-//   while (Serial.available()) {
-//     b = Serial.read();
-//     if (calcount == 0 && b != 117) {
-//       // first byte must be 117
-//       return;
-//     }
-//     if (calcount == 1 && b != 84) {
-//       // second byte must be 84
-//       calcount = 0;
-//       return;
-//     }
-//     // store this byte
-//     caldata[calcount++] = b;
-//     if (calcount < 68) {
-//       // full calibration message is 68 bytes
-//       return;
-//     }
-//     // verify the crc16 check
-//     crc = 0xFFFF;
-//     for (i=0; i < 68; i++) {
-//       crc = crc16_update(crc, caldata[i]);
-//     }
-//     if (crc == 0) {
-//       // data looks good, use it
-//       float offsets[16];
-//       memcpy(offsets, caldata+2, 16*4);
-//       cal.accel_zerog[0] = offsets[0];
-//       cal.accel_zerog[1] = offsets[1];
-//       cal.accel_zerog[2] = offsets[2];
-      
-//       cal.gyro_zerorate[0] = offsets[3];
-//       cal.gyro_zerorate[1] = offsets[4];
-//       cal.gyro_zerorate[2] = offsets[5];
-      
-//       cal.mag_hardiron[0] = offsets[6];
-//       cal.mag_hardiron[1] = offsets[7];
-//       cal.mag_hardiron[2] = offsets[8];
-
-//       cal.mag_field = offsets[9];
-      
-//       cal.mag_softiron[0] = offsets[10];
-//       cal.mag_softiron[1] = offsets[13];
-//       cal.mag_softiron[2] = offsets[14];
-//       cal.mag_softiron[3] = offsets[13];
-//       cal.mag_softiron[4] = offsets[11];
-//       cal.mag_softiron[5] = offsets[15];
-//       cal.mag_softiron[6] = offsets[14];
-//       cal.mag_softiron[7] = offsets[15];
-//       cal.mag_softiron[8] = offsets[12];
-
-//       if (! cal.saveCalibration()) {
-//         Serial.println("**WARNING** Couldn't save calibration");
-//       } else {
-//         Serial.println("Wrote calibration");    
-//       }
-//       cal.printSavedCalibration();
-//       calcount = 0;
-//       return;
-//     }
-//     // look for the 117,84 in the data, before discarding
-//     for (i=2; i < 67; i++) {
-//       if (caldata[i] == 117 && caldata[i+1] == 84) {
-//         // found possible start within data
-//         calcount = 68 - i;
-//         memmove(caldata, caldata + i, calcount);
-//         return;
-//       }
-//     }
-//     // look for 117 in last byte
-//     if (caldata[67] == 117) {
-//       caldata[0] = 117;
-//       calcount = 1;
-//     } else {
-//       calcount = 0;
-//     }
-//   }
-// }
-
-
-// uint16_t crc16_update(uint16_t crc, uint8_t a)
-// {
-//   int i;
-//   crc ^= a;
-//   for (i = 0; i < 8; i++) {
-//     if (crc & 1) {
-//       crc = (crc >> 1) ^ 0xA001;
-//     } else {
-//       crc = (crc >> 1);
-//     }
-//   }
-//   return crc;
-// }
